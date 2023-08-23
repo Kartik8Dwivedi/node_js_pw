b390f5ece2048110071d42157ad2de4060838292
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-08-23 12:30:50 +0530

    Code updated

---
+++ tailwindassignmentpw/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
